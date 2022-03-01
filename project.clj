2afd92eb4ac62402957d29946c349ce0e2bdbcdd
niloofar-samei <niloofar.samei@gmail.com>
2022-03-02 00:14:51 +0330

    Fixing typo

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [incise "0.5.0."]]
-  :aliases {"incise" ^:pass-through-help ["run" "-m" "incise.core"]}
+                 [incise "0.5.0"]]
+  :aliases {"incise" ^:pass-through-help ["run" "-m" "personal-blog.core"]}
   :main ^:skip-aot personal-blog.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
